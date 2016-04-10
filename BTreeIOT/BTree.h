635b8d05bd475a309c20850fc3fc636d04170f1c
Adnan Shaheen <shaheen4qau@gmail.com>
2016-04-10 19:08:51 -0400

    insert a node, definition only

--- BTreeIOT/BTree.h
+++ BTreeIOT/BTree.h
@@ -15,3 +15,27 @@ public:
 	CNode* m_pNext;			/* Pointer to next node */
 };
 
+/*
+ * CBTree class
+ */
+class CBTree
+{
+private:
+	CNode* m_pRoot;			/* root node of btree */
+
+public:
+	CBTree();				/* constructor */
+	~CBTree();				/* destructor */
+
+	/* Insert a node */
+	CNode* Insert(int nKey, int nValue);
+
+private:
+	CNode* Insert(CNode* pNode, int nKey, int nValue);			/* insert helper */
+	int* MakeRecord(int nValue);								/* make a record */
+	int GetLeftIndex(CNode* pParent, CNode* pLeft);				/* get the left most index */
+	CNode* MakeNode();											/* make a normal node */
+	CNode* MakeRoot();											/* make a root node */
+	CNode* InsertInLeaf(CNode* pNode, int nKey, int* pPointer);	/* insert at leaf */
+
+};
