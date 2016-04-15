b5b6441bda1ca74de6900a2a3d964336aee751c2
Adnan Shaheen <shaheen4qau@gmail.com>
2016-04-15 16:47:54 -0400

    input commands

--- BTreeIOT/Support.h
+++ BTreeIOT/Support.h
@@ -2,3 +2,15 @@
 
 const int nMinOrder = 3;
 const int nMaxOrder = 20;
+
+enum eCOMMANDS {
+	eNone = 0x0000,			/* no command specified by user */
+	eSelect = 0x0001,		/* select command */
+	eInsert = 0x0002,		/* insert command */
+	eDelete = 0x0004,		/* delete command */
+};
+struct OPTIONS {
+	uint32_t nOrder;		/* order of B+Tree */
+	eCOMMANDS eCommand;		/* command (eCOMMANDS) */
+	int nKEY;				/* primary key (for lookup and delete) */
+};
--- BTreeIOT/main.cpp
+++ BTreeIOT/main.cpp
@@ -1,13 +1,115 @@
 #include <iostream>
+#include <string>
 
 #include "Support.h"
 #include "BTree.h"
 
-int main()
+struct OPTIONS Opts;
+
+void Usage()
 {
+	std::cout << "Usage: BPlusTree [options]" << std::endl;
+	std::cout << std::endl;
+	std::cout << "--order number" << std::endl;
+}
+
+int parse_options(int argc, char* argv[])
+{
+	int nRes = 0;
+	try {
+		if (!argv || argc == 0)
+			throw std::exception("Please provide arguments");
+
+		int nArgs = 1;
+		if (argv[nArgs] && _stricmp(argv[nArgs++], "--order") == 0) {
+			Opts.nOrder = atoi(argv[nArgs++]);
+		}
+
+		if (argv[nArgs] && _stricmp(argv[nArgs++], "--select") == 0) {				/* select command */
+			Opts.eCommand = eSelect;
+			++argv;
+			/* at the moment we only provide key */
+			Opts.nKEY = atoi(argv[nArgs++]);
+		}
+
+		if (argv[nArgs] && _stricmp(argv[nArgs++], "--delete") == 0) {				/* delete command */
+			Opts.eCommand = eDelete;
+			++argv;
+			/* at the moment we only provide key */
+			Opts.nKEY = atoi(argv[nArgs++]);
+		}
+	}
+	catch (const std::exception& ex) {
+		std::cerr << ex.what() << std::endl;
+		Usage();
+	}
+	return nRes;
+}
+
+int main(int argc, char* argv[])
+{
+	if (parse_options(argc, argv))
+		return 0;
+
+
 	CBTree cBTreeObj;
 
-	cBTreeObj.SetOrder(nMinOrder);
+	if (Opts.nOrder < 3 || Opts.nOrder > 20) {
+		std::cerr << "Order not supported!!!" << std::endl;
+		return 0;
+	}
+
+	cBTreeObj.SetOrder(Opts.nOrder);
+
+	while (true) {
+		if (Opts.eCommand == eNone) {
+			std::cout << "Available Commands" << std::endl;
+			std::cout << "insert key" << std::endl;
+			std::cout << "delete key" << std::endl;
+			std::cout << "select key" << std::endl;
+			std::cout << "exit" << std::endl;
+			std::cout << std::endl;
+
+			std::string csCommand;
+			//std::cin.ignore();
+			std::getline(std::cin, csCommand);
+			if (_strnicmp(csCommand.c_str(), "select", strlen("select")) == 0) {
+				/* select command */
+				size_t nIndex = csCommand.rfind(" ");
+				Opts.nKEY = atoi(csCommand.substr(nIndex).c_str());
+				Opts.eCommand = eSelect;
+			}
+			else if (_strnicmp(csCommand.c_str(), "delete", strlen("delete")) == 0) {
+				/* delete command */
+				size_t nIndex = csCommand.rfind(" ");
+				Opts.nKEY = atoi(csCommand.substr(nIndex).c_str());
+				Opts.eCommand = eDelete;
+			}
+			else if (_strnicmp(csCommand.c_str(), "insert", strlen("insert")) == 0) {
+				/* insert command */
+				size_t nIndex = csCommand.rfind(" ");
+				Opts.nKEY = atoi(csCommand.substr(nIndex).c_str());
+				Opts.eCommand = eInsert;
+			}
+			else if (_strnicmp(csCommand.c_str(), "exit", strlen("exit")) == 0)
+				break;
+			else {
+				std::cerr << "Invalid command" << std::endl;
+				Usage();
+			}
+		}
+
+		if (Opts.eCommand == eInsert)					/* insert command */
+			cBTreeObj.Insert(Opts.nKEY, Opts.nKEY);
+		else if (Opts.eCommand == eDelete)				/* delete command */
+			cBTreeObj.Delete(Opts.nKEY);
+		else if (Opts.eCommand == eSelect)				/* select command */
+			cBTreeObj.Find(Opts.nKEY);
+
+		Opts.eCommand = eNone;							/* reset the command, to get next command */
+
+	}
+#ifdef _DEBUG
 	cBTreeObj.Insert(20, 20);
 	cBTreeObj.Insert(20, 20);
 	cBTreeObj.Insert(10, 10);
@@ -16,6 +118,6 @@ int main()
 	cBTreeObj.Find(10);
 	cBTreeObj.Delete(10);
 	cBTreeObj.Delete(20);
-
+#endif // _DEBUG
 	return 0;
 }
